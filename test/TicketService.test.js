25dfdc298036176740f04282b588f839a07b852c
Raghav Mittal <mittalraghav45@gmail.com>
2026-05-02 16:35:15 +0100

    ..

--- test/TicketService.test.js
+++ test/TicketService.test.js
@@ -2,7 +2,7 @@ import TicketService from "../src/pairtest/TicketService.js";
 import TicketTypeRequest from "../src/pairtest/lib/TicketTypeRequest.js";
 import InvalidPurchaseException from "../src/pairtest/lib/InvalidPurchaseException.js";
 
-// test case to check invalid id
+// test cases
 test("check for valid account id", () => {
   const service = new TicketService();
 
